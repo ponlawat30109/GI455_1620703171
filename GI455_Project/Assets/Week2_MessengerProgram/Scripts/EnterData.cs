dbb658181a0077c3dd0ce46573759603290a991e
ponlawat suwanyukabordin <ponlawat30109@users.noreply.github.com>
2021-02-04 02:19:08 +0700

    Send Assignment Week2

---
+++ GI455_Project/Assets/Week2_MessengerProgram/Scripts/EnterData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace MessengerProgram
+{
+    public class EnterData : MonoBehaviour
+    {
+        public GameObject urlInput, portInput, nameInput;
+        public Button connectButton;
+        public static string url, port, username;
+
+        private void Start()
+        {
+            connectButton.onClick.AddListener(Connect);
+
+            //string url = urlInput.GetComponent<Text>().text;
+            //string port = portInput.GetComponent<Text>().text;
+            //string name = nameInput.GetComponent<Text>().text;
+            //if (url == "")
+            //{
+            //    url = "127.0.0.1";
+            //}
+
+            //if (port == "")
+            //{
+            //    port = "5500";
+            //}
+
+            //if (name == "")
+            //{
+            //    name = "Anonymous";
+            //}
+
+            //print($"{url}:{port}");
+        }
+
+        public void Connect()
+        {
+            url = urlInput.GetComponent<Text>().text;
+            port = portInput.GetComponent<Text>().text;
+            username = nameInput.GetComponent<Text>().text;
+            if (url == "")
+            {
+                url = "127.0.0.1";
+            }
+
+            if (port == "")
+            {
+                port = "5500";
+            }
+
+            if (username != "")
+            {
+                print($"{username} : {url}:{port}");
+                SceneManager.LoadScene("MessengerProgram");
+            }
+        }
+    }
+}
---
+++ GI455_Project/Assets/Week2_MessengerProgram/Scripts/WebsocketConnection.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using WebSocketSharp;
+
+namespace MessengerProgram
+{
+    public class WebsocketConnection : MonoBehaviour
+    {
+        private WebSocket websocket;
+
+        string url = EnterData.url;
+        string port = EnterData.port;
+        string username = EnterData.username;
+        string textMessage = "";
+
+        public InputField inputField;
+        public Text chatText, headerText;
+        public Button sendButton, leaveButton;
+
+        public Canvas chatCanvas;
+
+        public Transform content;
+        public GameObject newChat;
+
+        [SerializeField] List<string> message = new List<string>();
+
+        void Start()
+        {
+            headerText.text = $"Room ws://{url}:{port}/";
+
+            websocket = new WebSocket($"ws://{url}:{port}/");
+
+            websocket.OnMessage += OnMessage;
+
+            websocket.Connect();
+
+            //chatText.text = string.Empty;
+            chatText.text = $"\n<b>Welcome {username} to GI455 Chat</b>\n";
+            chatText.alignment = TextAnchor.UpperCenter;
+
+            //Text newTextbox = Instantiate(chatText, content) as Text;
+            //newTextbox.transform.SetParent(content.transform);
+            //newTextbox.text = string.Empty;
+
+            //CreateNewChat();
+
+            sendButton.onClick.AddListener(GetText);
+            leaveButton.onClick.AddListener(LeaveChat);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                GetText();
+            }
+        }
+
+        //public void CreateNewChat()
+        //{
+        //    Text newTextbox = Instantiate(chatText, content) as Text;
+        //    newTextbox.transform.SetParent(content.transform);
+        //    newTextbox.text += message[message.Count - 1];
+        //}
+
+        public void ChatUpdate()
+        {
+            //chatText.text += message[message.Count - 1] + "\n";
+
+            Text newTextbox = Instantiate(chatText, content) as Text;
+            newTextbox.transform.SetParent(content.transform);
+            newTextbox.text = string.Empty;
+
+            string[] temp = message[message.Count - 1].Split(':');
+
+            newTextbox.alignment = TextAnchor.UpperLeft;
+            if (temp[0] == username)
+            {
+                newTextbox.alignment = TextAnchor.UpperRight;
+                int usernameStrLenght = temp[0].Length;
+                //string newTemp = message[message.Count - 1].Substring(usernameStrLenght);
+                newTextbox.text += message[message.Count - 1].Substring(usernameStrLenght + 2) + "\n";
+            }
+            else
+            {
+                newTextbox.text += message[message.Count - 1] + "\n";
+            }
+
+        }
+
+        private void OnDestroy()
+        {
+            if (websocket != null)
+            {
+                websocket.Close();
+            }
+        }
+
+        public void GetText()
+        {
+            textMessage = inputField.text;
+            inputField.text = string.Empty;
+
+            if (textMessage != "")
+            {
+                if (websocket.ReadyState == WebSocketState.Open)
+                {
+                    string currentMessage = string.Concat($"{username}: {textMessage}");
+                    websocket.Send($"{currentMessage}");
+                }
+
+            }
+
+            //ChatUpdate();
+            //CreateNewChat();
+        }
+
+        public void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            Debug.Log("Recieve msg from " + messageEventArgs.Data);
+
+            message.Add(messageEventArgs.Data);
+
+            ChatUpdate();
+        }
+
+        public void LeaveChat()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+        }
+    }
+}
